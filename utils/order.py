698750f8f872acadf2e979fc617413c313c813a9
Nguyen Ngoc <knkidngoc@gmail.com>
2025-07-18 01:19:33 +0700

    add logic handle

--- utils/order.py
+++ utils/order.py
@@ -1,2 +1,4 @@
 def reorder_images(uploaded_files: list, order: list) -> list:
+    if not all(0 <= i < len(uploaded_files) for i in order):
+        raise ValueError("Invalid Order Index")
     return [uploaded_files[i] for i in order]
